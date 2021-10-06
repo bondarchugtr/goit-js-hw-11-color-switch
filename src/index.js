5f6ddf13313f4b91e3f55b73f6709cc60b71fed1
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-10-07 01:56:30 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import refs from '../src/js/refs.js';
+
+const { startBtn, stopBtn, body, button } = refs
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+startBtn.addEventListener('click', onClickStartBtn)
+stopBtn.addEventListener('click', onClickStopBtn)
+
+let timeId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function backGroundColorRgb(evt) {
+    body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+
+    button.forEach((el) => {
+        el.style.width = 100 + 'px'
+        el.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length + 1)];
+        el.style.height = 50 + 'px'
+        el.style.borderRadius = 5 + 'px'
+    })
+}
+
+function onClickStartBtn(evt) {
+    if (evt.target) {
+        startBtn.setAttribute("disabled", "disabled")
+        timeId = setInterval(backGroundColorRgb, 1000)
+        console.log(timeId)
+    }
+}
+
+function onClickStopBtn(evt) {
+    if (evt.target) {
+        clearInterval(timeId)
+        startBtn.removeAttribute("disabled", "disabled")
+    }
+}
+
+
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+
+export default {
+    startBtn: document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+    body: document.querySelector('body'),
+    button: document.querySelectorAll('button')
+}
